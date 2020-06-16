8d679efa26152486cfbac69576657b635f036fbc
Coleman Platt <9168088+MCplatt@users.noreply.github.com>
2020-06-16 15:39:51 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -270,7 +270,7 @@ def colorChange(strip, stripNext, Frames, delay, mod = 0):
 #https://agromonitoring.com/api/current-weather
 def APIgetWeather(param1,param2,location):
     URL = "https://api.openweathermap.org/data/2.5/weather"
-    apikey = "5e62e6d5676f5954155893401110d3f2"
+    apikey = ""
     payload = {'q':location,'appid':apikey}
     time.sleep(15) #only allow 1 request per second
 
